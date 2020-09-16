c6f2f291558e2c3e916d2f54fcf53cbdec9f7c0c
Hammad <hammad@ahmed.com>
2020-09-16 18:17:41 +0500

    Updates

---
+++ precache-manifest.42bcb003b3e8b7ad96759188ebb2fc66.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c601e8c6cf079249a66626c7ea4802d6",
+    "url": "/RoboFriends/index.html"
+  },
+  {
+    "revision": "8bfacd2c578469929829",
+    "url": "/RoboFriends/static/css/2.90f3f635.chunk.css"
+  },
+  {
+    "revision": "6b8e73948d00d7bf3f0c",
+    "url": "/RoboFriends/static/css/main.93697a08.chunk.css"
+  },
+  {
+    "revision": "8bfacd2c578469929829",
+    "url": "/RoboFriends/static/js/2.43b62a47.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RoboFriends/static/js/2.43b62a47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b8e73948d00d7bf3f0c",
+    "url": "/RoboFriends/static/js/main.1dc23b37.chunk.js"
+  },
+  {
+    "revision": "91966aaed0b807750b97",
+    "url": "/RoboFriends/static/js/runtime-main.cb0f80e8.js"
+  }
+]);
